c1a7cea02827cf4a4c523ca2d1280c06ba85c489
gauravidya <gaurav.meena@vidyagxp.com>
2024-05-28 18:51:31 +0530

    changes

--- src/pages/Dashboard/Accounting/Reports/Receivables/Receivables.jsx
+++ src/pages/Dashboard/Accounting/Reports/Receivables/Receivables.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import styles from "../Reports.module.css";
 import Tab from "../../../../../Components/Tabs/Tabs";
-export default function Receivables() {
+import {
+  customerBalanceData,
+  receivableSummaryData,
+  receivableDetailsData,
+  agingSummaryData,
+  agingDetailData,
+} from "./ReceivableFunctions.jsx";
+
+const Receivables = () => {
   const CustomerBalance = (
     <div className={styles.reportTableContainer}>
       <div className={styles.tableTop}>
@@ -21,43 +29,21 @@ export default function Receivables() {
       <table className={styles.dashboardTable}>
         <thead>
           <tr>
-            <th>CUSTOMER NAME </th>
-            <th>INVOICE BALANCE </th>
-            <th>AVAILABLE CREDITS </th>
+            <th>CUSTOMER NAME</th>
+            <th>INVOICE BALANCE</th>
+            <th>AVAILABLE CREDITS</th>
             <th>BALANCE</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-          </tr>
-          <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-          </tr>
-          <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-          </tr>
-          <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-          </tr>
-          <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-          </tr>
+          {customerBalanceData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.invoiceBalance}</td>
+              <td>{item.availableCredits}</td>
+              <td>{item.balance}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className={styles.tableBottom}>
@@ -85,43 +71,177 @@ export default function Receivables() {
       <table className={styles.dashboardTable}>
         <thead>
           <tr>
-            <th>CUSTOMER NAME </th>
-            <th>INVOICE COUNT </th>
-            <th>SALES</th>
-            <th> SALES WITH TAX </th>
+            <th>CUSTOMER NAME</th>
+            <th>DATE</th>
+            <th>TRANSACTION</th>
+            <th>STATUS</th>
+            <th>TRANSACTION TYPE</th>
+            <th>TOTAL</th>
+            <th>BALANCE</th>
           </tr>
         </thead>
         <tbody>
+          {receivableSummaryData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.date}</td>
+              <td>{item.transaction}</td>
+              <td>{item.status}</td>
+              <td>{item.type}</td>
+              <td>{item.total}</td>
+              <td>{item.balance} </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className={styles.tableBottom}>
+        <p>Showing 1 to 5 of 5 entries</p>
+      </div>
+    </div>
+  );
+
+  const ReceivableDetails = (
+    <div className={styles.reportTableContainer}>
+      <div className={styles.tableTop}>
+        <div className={styles.tableTopleft}>
+          <select>
+            <option>5</option>
+            <option>10</option>
+            <option>15</option>
+            <option>20</option>
+          </select>
+          <p>entries per Page</p>
+        </div>
+        <div className={styles.field}>
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <table className={styles.dashboardTable}>
+        <thead>
           <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
+            <th>CUSTOMER NAME</th>
+            <th>DATE</th>
+            <th>TRANSACTION</th>
+            <th>STATUS</th>
+            <th>TRANSACTION TYPE</th>
+            <th>ITEM NAME</th>
+            <th>QUANTITY ORDERED</th>
+            <th>ITEM PRICE</th>
+            <th>TOTAL</th>
           </tr>
+        </thead>
+        <tbody>
+          {receivableDetailsData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.date}</td>
+              <td>{item.transaction}</td>
+              <td>{item.status}</td>
+              <td>{item.type}</td>
+              <td>{item.item}</td>
+              <td>{item.quantity}</td>
+              <td>{item.price}</td>
+              <td>{item.total}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className={styles.tableBottom}>
+        <p>Showing 1 to 5 of 5 entries</p>
+      </div>
+    </div>
+  );
+
+  const AgingSummary = (
+    <div className={styles.reportTableContainer}>
+      <div className={styles.tableTop}>
+        <div className={styles.tableTopleft}>
+          <select>
+            <option>5</option>
+            <option>10</option>
+            <option>15</option>
+            <option>20</option>
+          </select>
+          <p>entries per Page</p>
+        </div>
+        <div className={styles.field}>
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <table className={styles.dashboardTable}>
+        <thead>
           <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
+            <th>CUSTOMER NAME</th>
+            <th>CURRENT</th>
+            <th>1-15 DAYS</th>
+            <th>16-30 DAYS</th>
+            <th>31-45 DAYS</th>
+            <th> 45 DAYS</th>
+            <th>TOTAL</th>
           </tr>
+        </thead>
+        <tbody>
+          {agingSummaryData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.current}</td>
+              <td>{item.days1_15}</td>
+              <td>{item.days16_30}</td>
+              <td>{item.days31_45}</td>
+              <td>{item.over45}</td>
+              <td>{item.total}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className={styles.tableBottom}>
+        <p>Showing 1 to 5 of 5 entries</p>
+      </div>
+    </div>
+  );
+
+  const AgingDetail = (
+    <div className={styles.reportTableContainer}>
+      <div className={styles.tableTop}>
+        <div className={styles.tableTopleft}>
+          <select>
+            <option>5</option>
+            <option>10</option>
+            <option>15</option>
+            <option>20</option>
+          </select>
+          <p>entries per Page</p>
+        </div>
+        <div className={styles.field}>
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <table className={styles.dashboardTable}>
+        <thead>
           <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
+            <th>DATE</th>
+            <th>TRANSACTION</th>
+            <th>TYPE</th>
+            <th>STATUS</th>
+            <th>CUSTOMER NAME</th>
+            <th>AGE</th>
+            <th>AMOUNT</th>
+            <th>BALANCE DUE</th>
           </tr>
-          <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-          </tr>
-          <tr>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-            <td>content</td>
-          </tr>
+        </thead>
+        <tbody>
+          {agingDetailData.map((item, index) => (
+            <tr key={index}>
+              <td>{item.date}</td>
+              <td>{item.transaction}</td>
+              <td>{item.type}</td>
+              <td>{item.status}</td>
+              <td>{item.name}</td>
+              <td>{item.age}</td>
+              <td>{item.amount}</td>
+              <td>{item.balanceDue}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <div className={styles.tableBottom}>
@@ -132,10 +252,10 @@ export default function Receivables() {
 
   const tabs = [
     { label: "Customer Balance", content: CustomerBalance },
-    { label: "Receivable Summary ", content: ReceivableSummary },
-    { label: "Receivable Details ", content: CustomerBalance },
-    { label: "Aging Summary ", content: ReceivableSummary },
-    { label: "Aging Details ", content: CustomerBalance },
+    { label: "Receivable Summary", content: ReceivableSummary },
+    { label: "Receivable Details", content: ReceivableDetails },
+    { label: "Aging Summary", content: AgingSummary },
+    { label: "Aging Details", content: AgingDetail },
   ];
 
   return (
@@ -149,4 +269,6 @@ export default function Receivables() {
       </div>
     </div>
   );
-}
+};
+
+export default Receivables;
